38949663eebb1b01c54887881af2a0c4f97e031f
robobes <109526122+robobes@users.noreply.github.com>
2023-12-26 10:15:03 +0300

    Regular auto-commit at 10:15:03 on 26/12/2023

--- CODE/GATHER_DATA/gather_tefas.py
+++ CODE/GATHER_DATA/gather_tefas.py
@@ -5,14 +5,14 @@ Created on Fri Dec 22 16:46:26 2023
 @author: PC
 """
 from tefas import Crawler
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 import pyarrow as pa
 import pyarrow.dataset as ds
 
 
 tefas = Crawler()
-table = pa.Table.from_pandas(pd.DataFrame(tefas.fetch(start=datetime.today())))
+table = pa.Table.from_pandas(pd.DataFrame(tefas.fetch(start=datetime.today()-timedelta(days=5),end=datetime.today())))
 ds.write_dataset(table, "./DATA/DATABASE/TEFAS",
                  format="parquet", partitioning=ds.partitioning(
                     pa.schema([table.schema.field("date")])),existing_data_behavior="overwrite_or_ignore")
