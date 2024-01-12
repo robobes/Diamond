5b0989293900f77a83053160f3f8896a7ef519f7
robobes <109526122+robobes@users.noreply.github.com>
2024-01-12 19:17:17 +0300

    Regular auto-commit at 19:17:17 on 12/01/2024

--- CODE/GATHER_DATA/gather_kyd.py
+++ CODE/GATHER_DATA/gather_kyd.py
@@ -4,17 +4,22 @@ Created on Fri Jan 12 16:39:29 2024
 
 @author: PC
 """
-import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
+
+
+
+import pandas as pd
 import time
 import pyarrow as pa
 import pyarrow.dataset as ds
-
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
