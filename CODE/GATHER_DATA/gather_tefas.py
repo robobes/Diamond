7d72eb9c7aca66e58395542f3a3cbd17e28d3e35
robobes <109526122+robobes@users.noreply.github.com>
2024-02-05 11:10:54 +0300

    Update gather_tefas.py

--- CODE/GATHER_DATA/gather_tefas.py
+++ CODE/GATHER_DATA/gather_tefas.py
@@ -13,7 +13,24 @@ import pyarrow.dataset as ds
 
 
 tefas = Crawler()
-table = pa.Table.from_pandas(pd.DataFrame(tefas.fetch(start=datetime.today()-timedelta(days=5),end=datetime.today())))
+
+df = pd.DataFrame(tefas.fetch(start=datetime.today()-timedelta(days=5),end=datetime.today()))
+
+df['Date'] = pd.to_datetime(df['Date'])
+df["mon"] = pd.DatetimeIndex(df['Date']).month
+df["year"] = pd.DatetimeIndex(df['Date']).year
+df["yearmon"] = df["year"].apply(str) +"_"+ df["mon"].apply(str)
+df = df.drop(["mon","year"],axis=1)
+
+df["update"]=2
+
+dat = ds.dataset("./DATA/DATABASE/TEFAS",partitioning=["yearmon"])
+dat["update"]=1
+
+dataf=pd.concat([df,dat],ignore_index=True).sort_values('update', ascending=False).drop_duplicates(["code","Date"]).drop(["update"],axis=1)
+dataf.reset_index().drop("index",axis=1)
+
+table = pa.Table.from_pandas(dataf)
 ds.write_dataset(table, "./DATA/DATABASE/TEFAS",
                  format="parquet", partitioning=ds.partitioning(
-                    pa.schema([table.schema.field("date")])),existing_data_behavior="overwrite_or_ignore")
+                    pa.schema([table.schema.field("yearmon")])),existing_data_behavior="overwrite_or_ignore")
