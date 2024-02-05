4df794f3df42d21f75f38ccb14cd60aff7f7394f
robobes <109526122+robobes@users.noreply.github.com>
2024-02-05 11:18:55 +0300

    Regular auto-commit at 11:18:55 on 05/02/2024

--- CODE/GATHER_DATA/gather_tefas.py
+++ CODE/GATHER_DATA/gather_tefas.py
@@ -16,9 +16,9 @@ tefas = Crawler()
 
 df = pd.DataFrame(tefas.fetch(start=datetime.today()-timedelta(days=5),end=datetime.today()))
 
-df['Date'] = pd.to_datetime(df['Date'])
-df["mon"] = pd.DatetimeIndex(df['Date']).month
-df["year"] = pd.DatetimeIndex(df['Date']).year
+df['date'] = pd.to_datetime(df['date'])
+df["mon"] = pd.DatetimeIndex(df['date']).month
+df["year"] = pd.DatetimeIndex(df['date']).year
 df["yearmon"] = df["year"].apply(str) +"_"+ df["mon"].apply(str)
 df = df.drop(["mon","year"],axis=1)
 
@@ -27,7 +27,7 @@ df["update"]=2
 dat = ds.dataset("./DATA/DATABASE/TEFAS",partitioning=["yearmon"])
 dat["update"]=1
 
-dataf=pd.concat([df,dat],ignore_index=True).sort_values('update', ascending=False).drop_duplicates(["code","Date"]).drop(["update"],axis=1)
+dataf=pd.concat([df,dat],ignore_index=True).sort_values('update', ascending=False).drop_duplicates(["code","date"]).drop(["update"],axis=1)
 dataf.reset_index().drop("index",axis=1)
 
 table = pa.Table.from_pandas(dataf)
