e1abf8e513288042ae434e81dd5f9f3691f708a0
robobes <109526122+robobes@users.noreply.github.com>
2024-03-13 17:35:24 +0300

    Update app.py

--- CODE/DASH/app.py
+++ CODE/DASH/app.py
@@ -11,8 +11,8 @@ import pandas as pd
 import os
 
 
-regime_dat =  pd.read_csv( os.path.join('regime_data.csv') )
-#regime_dat = pd.read_csv("C:/Users/PC/Documents/Github/Diamond/DATA/SHINY/regime_data.csv")
+#regime_dat =  pd.read_csv( os.path.join('regime_data.csv') )
+regime_dat = pd.read_csv("C:/Users/PC/Documents/Github/Diamond/DATA/SHINY/regime_data.csv")
 
 regime_colors = {
     "DISINFLATION": "rgb(85, 107, 47)",
@@ -38,10 +38,37 @@ server = app.server
 
 app.layout = html.Div([
     html.H1(children='Title of Dash App1', style={'textAlign':'center'}),
-    dcc.Dropdown(regime_dat.Country.unique(), 'US', id='dropdown-selection-country'),
-    dcc.Dropdown(regime_dat.Field.unique(), 'INF_YOY', id='dropdown-selection-field'),
+    dcc.Dropdown(regime_dat.Country.unique(), None, id='dropdown-selection-country'),
+    dcc.Dropdown([], None,disabled=True, id='dropdown-selection-field'),
     dcc.Graph(id='graph-content')
 ])
+
+
+
+
+
+
+@callback(
+    [Output("dropdown-selection-field","options"),
+    Output("dropdown-selection-field","disabled")],
+    Input("dropdown-selection-country","value")    
+)
+def second_dropdown(country):
+    if country:
+        opts = regime_dat[(regime_dat.Country==country)].Field.unique()
+        return opts,False
+    else:
+        return [],True
+
+
+
+
+
+
+
+
+
+
 
 
 @callback(
@@ -50,33 +77,36 @@ app.layout = html.Div([
     Input('dropdown-selection-country', 'value')]
 )
 def update_graph(field,country):
-    dff = regime_dat[(regime_dat.Field==field) & (regime_dat.Country==country)]
-    dff = dff[["Date","Value","Regime"]].dropna().reset_index(drop=True)
-    fig= px.line(dff, x="Date", y="Value")
-    fig.update_traces(line_color='#000000')
-
-    ply_shapes = {}
-    for i in range(1, len(dff)):
-        ply_shapes['shape_' + str(i)]=go.layout.Shape(type="rect",
-                                                        x0=dff.Date[i-1], x1=dff.Date[i],y0=dff.Value.max(),y1=dff.Value.min(),
-                                                        fillcolor=regime_colors[dff["Regime"][i]].replace('rgb', 'rgba').rstrip(')') + ',0.6)',
-                                                        layer="below", line_width=0.5,
-                                                        line_color=regime_colors[dff["Regime"][i]].replace('rgb', 'rgba').rstrip(')') + ',0.1)'
-                                                    )
-    lst_shapes=list(ply_shapes.values())
-    fig.update_layout(shapes=lst_shapes)
+    if country and field:
+        dff = regime_dat[(regime_dat.Field==field) & (regime_dat.Country==country)]
+        dff = dff[["Date","Value","Regime"]].dropna().reset_index(drop=True)
+        fig= px.line(dff, x="Date", y="Value")
+        fig.update_traces(line_color='#000000')
     
-    fig.update_layout(
-        title="Value Over Time by Regime",
-        xaxis_title="Date",
-        yaxis_title="Value",
-        plot_bgcolor="rgba(0,0,0,0)",
-    )
-    fig.update_xaxes(showgrid=False)
-    fig.update_yaxes(showgrid=False)
-
-    return fig
-
+        ply_shapes = {}
+        for i in range(1, len(dff)):
+            ply_shapes['shape_' + str(i)]=go.layout.Shape(type="rect",
+                                                            x0=dff.Date[i-1], x1=dff.Date[i],y0=dff.Value.max(),y1=dff.Value.min(),
+                                                            fillcolor=regime_colors[dff["Regime"][i]].replace('rgb', 'rgba').rstrip(')') + ',0.6)',
+                                                            layer="below", line_width=0.5,
+                                                            line_color=regime_colors[dff["Regime"][i]].replace('rgb', 'rgba').rstrip(')') + ',0.1)'
+                                                        )
+        lst_shapes=list(ply_shapes.values())
+        fig.update_layout(shapes=lst_shapes)
+        
+        fig.update_layout(
+            title="Value Over Time by Regime",
+            xaxis_title="Date",
+            yaxis_title="Value",
+            plot_bgcolor="rgba(0,0,0,0)",
+        )
+        fig.update_xaxes(showgrid=False)
+        fig.update_yaxes(showgrid=False)
+    
+        return fig
+    else:
+        return go.Figure()
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+    #app.run_server(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+    app.run(debug=True)
